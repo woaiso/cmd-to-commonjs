642e3732a9b00ecd02dc1620374e6c3af99b0495
yunful <yunful@jumei.com>
2017-03-22 10:38:37 +0800

    add comment

--- src/transform.ts
+++ src/transform.ts
@@ -10,16 +10,27 @@ export default function (file: any, api: any) {
             const functionExpression = path.node.expression.arguments[ 0 ];
             const comments = path.node.comments;
             const result = [];
-            if (comments && comments.length) {
-                comments.forEach(comment => {
-                    result.push(comment);
-                })
-
-            }
-            console.log(result);
             const leading = [];
             let isLeading = true;
-            functionExpression.body.body.forEach((item) => {
+            functionExpression.body.body.forEach((item, index) => {
+                //处理开头的注释
+                if (index === 0 && comments && comments.length) {
+                    const firstNode = item;
+                    firstNode.comments = [];
+                    comments.forEach((comment) => {
+                        switch (comment.type) {
+                            case 'CommentLine':
+                                firstNode.comments.push(jscodeshift.commentLine(
+                                    comment.value, comment.leading, comment.trailing));
+                                break;
+                            case 'CommentBlock':
+                                firstNode.comments.push(jscodeshift.commentBlock(
+                                    comment.value, comment.leading, comment.trailing));
+                                break;
+                        }
+                    });
+                    item= firstNode;
+                }
                 if (isLeading &&
                     item.type === 'ExpressionStatement' &&
                     item.expression.type === 'Literal') {
